0c8d0c24026896aed2452a4bad366ccebd147795
Ahmed Saber <ahmedsaber10@users.noreply.github.com>
2024-05-20 00:50:42 +0200

    Update playwright.config.ts
    run chrome and firefox only

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,10 +59,7 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+  
 
   ]   
 });
